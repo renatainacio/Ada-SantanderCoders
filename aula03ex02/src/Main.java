f208e8a8b12aac7cb5ef6babb33e1acaf620477c
renata <revazgauska@gmail.com>
2022-11-16 13:37:51 -0300

    aula03ex02

---
+++ aula03ex02/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean looping;
+        Integer idade;
+
+        idade = 0;
+        do {
+            try{
+                looping = false;
+                idade = Integer.parseInt(input.next());
+            } catch (NumberFormatException e){
+                looping = true;
+                System.out.println("Formato de idade inválido, insira novamente.");}
+        } while (looping == true);
+        System.out.println((idade >= 18 && idade <= 70) ? "Voto obrigatório" :
+                ((idade >= 16 || idade > 70) ? "Voto facultativo" : "Sem direito a votar"));
+    }
+}
