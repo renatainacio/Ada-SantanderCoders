04cace009ecda432e51ce48db81aedd7ed5ac25f
renata <revazgauska@gmail.com>
2022-11-30 21:44:59 -0300

    .gitignore

--- aula03ex02/src/Main.java
+++ aula03ex02/src/Main.java
@@ -16,7 +16,10 @@ public class Main {
                 looping = true;
                 System.out.println("Formato de idade inválido, insira novamente:");}
         } while (looping == true);
-        System.out.println((idade >= 18 && idade <= 70) ? "Voto obrigatório" :
-                ((idade >= 16 || idade > 70) ? "Voto facultativo" : "Sem direito a votar"));
+        System.out.println(checkIdade(idade));
+    }
+    private static String checkIdade (Integer idade) {
+        return ((idade >= 18 && idade <= 70) ?  "Voto obrigatório" :
+                ((idade >= 16 || idade > 70) ? "Voto facultativo" : "Sem direito a votar"))
     }
 }
